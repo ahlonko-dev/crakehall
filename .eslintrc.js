fe4936cc8a6b0d67e22b56fc70813b9703aa41ed
bertholddovi <bertholddovi@gmail.com>
2020-07-08 13:33:04 +0200

    add projet

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+/* becodeorg/mwenbwa
+ *
+ * /webpack.config.js - Webpack configuration
+ *
+ * coded by leny@BeCode
+ * started at 18/05/2020
+ */
+
+module.exports = {
+    extends: "@becode",
+    rules: {
+        "react/button-has-type": 0,
+    },
+};
